30d3835c36c204137d390854935a0e201c65a477
eduardosaraujo1 <eduardo.araujo109@etec.sp.gov.br>
2025-05-22 18:41:44 -0300

    refactor: format

--- php/contato/store.php
+++ php/contato/store.php
@@ -2,14 +2,14 @@
 require '../core/autoload.php';
 use Core\Database\Connection;
 
-function uuidv4()
+function uuidv4(): string
 {
-  $data = random_bytes(16);
+    $data = random_bytes(16);
 
-  $data[6] = chr(ord($data[6]) & 0x0f | 0x40); // set version to 0100
-  $data[8] = chr(ord($data[8]) & 0x3f | 0x80); // set bits 6-7 to 10
-    
-  return vsprintf('%s%s-%s-%s-%s-%s%s%s', str_split(bin2hex($data), 4));
+    $data[6] = chr(ord($data[6]) & 0x0f | 0x40); // set version to 0100
+    $data[8] = chr(ord($data[8]) & 0x3f | 0x80); // set bits 6-7 to 10
+
+    return vsprintf('%s%s-%s-%s-%s-%s%s%s', str_split(bin2hex($data), 4));
 }
 
 Connection::create([
@@ -30,13 +30,13 @@ $telefone = $_POST["telefone"];
 // store uploaded photo
 $photo_path = null;
 
-if (isset($_FILES["foto"]) && $_FILES["foto"]["error"] === UPLOAD_ERR_OK) {
+if (isset($_FILES["foto"]) && $_FILES["foto"]["error"] === 0) {
     $foto = $_FILES["foto"];
     $tmp = $foto["tmp_name"];
     $originalName = $foto["name"];
     $extension = strtolower(pathinfo($originalName, PATHINFO_EXTENSION));
 
-    $allowed = ["jpg", "jpeg", "png", "gif", "webp"];
+    $allowed = ["jpg", "jpeg", "png"];
     if (!in_array($extension, $allowed)) {
         die("Tipo de arquivo não permitido.");
     }
@@ -46,10 +46,6 @@ if (isset($_FILES["foto"]) && $_FILES["foto"]["error"] === UPLOAD_ERR_OK) {
     $relativePath = "/storage/contato/foto/" . $filename;
     $targetDir = $_SERVER['DOCUMENT_ROOT'] . "/storage/contato/foto/";
     $targetPath = $_SERVER['DOCUMENT_ROOT'] . $relativePath;
-
-    if (!is_dir($targetDir)) {
-        mkdir($targetDir, 0755, true);
-    }
 
     if (move_uploaded_file($tmp, $targetPath)) {
         $photo_path = $relativePath;
@@ -64,12 +60,12 @@ Connection::query(
 INSERT INTO tb_contato (nome, login, senha, email, telefone, foto) VALUES
 (:nome, :login, :senha, :email, :telefone, :foto)",
     params: [
-    "nome" => $nome,
-    "login" => $login,
-    "senha" => $senha,
-    "email" => $email,
-    "telefone" => $telefone,
-    "foto" => $photo_path
+        "nome" => $nome,
+        "login" => $login,
+        "senha" => $senha,
+        "email" => $email,
+        "telefone" => $telefone,
+        "foto" => $photo_path
     ]
 );
 
@@ -84,15 +80,3 @@ foreach ($users as $key => $value) {
 }
 
 var_dump($users);
-
-/**
-[WWWWWWWWWWWWWWWW]
-f(i) = offset + i - length
--length <= f(i) <= 
-0 <= offset <= length-1
-[WWWWWWWWWWWWWWWW]
-f(i) = 15 + i - offset
-<= f(i) <=
-<= offset <= 
- 
- */
