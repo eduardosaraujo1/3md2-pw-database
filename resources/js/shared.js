48a63bf700aecfd865d1013e4181da32df193196
eduardosaraujo1 <seraos.rodaguj@proton.me>
2025-08-14 17:58:49 -0300

    refactor: add shared logic to file

---
+++ resources/js/shared.js
@@ -0,0 +1,24 @@
+function sendFormData({ endpoint, method = "POST", formData, onSuccess, onError }) {
+    $.ajax({
+        url: endpoint,
+        method: method,
+        data: formData,
+        contentType: false,
+        processData: false,
+        xhrFields: {
+            withCredentials: true,
+        },
+        success: function (response) {
+            if (typeof onSuccess === "function") {
+                onSuccess(response);
+            }
+        },
+        error: function (xhr) {
+            if (typeof onError === "function") {
+                onError(xhr);
+            } else {
+                console.error("AJAX error:", xhr);
+            }
+        },
+    });
+}
