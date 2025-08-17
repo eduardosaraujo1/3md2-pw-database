9e79c0cf177dd2d65d1ebe0186b0bca03a37ab2b
eduardosaraujo1 <seraos.rodaguj@proton.me>
2025-08-17 03:25:41 -0300

    refactor: separate router file

--- index.php
+++ index.php
@@ -4,52 +4,13 @@ require 'core/autoload.php';
 require 'core/functions.php';
 
 use Core\Http\Kernel;
-use Core\Http\Response;
-use Core\Routing\Router;
-use App\Controllers\AuthController;
-use App\Controllers\UserController;
 use Core\Http\Request;
 
 // Bootstrap app service providers
 app()->bootstrap();
 
 // Router
-/** @var AuthController */
-$authController = app()->make(AuthController::class);
-/** @var UserController */
-$userController = app()->make(UserController::class);
-
-$router = app()->make(Router::class);
-
-// Auth
-$router->get('/login', function () use ($authController) {
-    return $authController->login();
-});
-$router->post('/signin', function (Request $request) use ($authController) {
-    return $authController->signin($request);
-});
-$router->get('/signout', function () use ($authController) {
-    return $authController->signout();
-});
-
-// User
-$router->get('/', function () use ($userController): Response {
-    return $userController->home();
-});
-$router->get('/users', function () use ($userController) {
-    return $userController->index();
-});
-$router->post('/users/store', function (Request $request) use ($userController) {
-    return $userController->store($request);
-});
-
-// Deprecated
-$router->get('/profile', function () use ($userController): Response {
-    return $userController->getProfile();
-});
-$router->post('/profile/update', function (Request $request) use ($userController): Response {
-    return $userController->updateProfile($request);
-});
+require 'routes/rest.php';
 
 // Request
 $request = app()->make(Request::class);
---
+++ routes/rest.php
@@ -0,0 +1,42 @@
+<?php
+use Core\Http\Response;
+use Core\Routing\Router;
+use App\Controllers\AuthController;
+use App\Controllers\UserController;
+use Core\Http\Request;
+
+$router = app()->make(Router::class);
+/** @var AuthController */
+$authController = app()->make(AuthController::class);
+/** @var UserController */
+$userController = app()->make(UserController::class);
+
+// Auth
+$router->get('/login', function () use ($authController): Response {
+    return $authController->login();
+});
+$router->post('/signin', function (Request $request) use ($authController): Response {
+    return $authController->signin($request);
+});
+$router->get('/signout', function () use ($authController): Response {
+    return $authController->signout();
+});
+
+// User
+$router->get('/', function () use ($userController): Response {
+    return $userController->home();
+});
+$router->get('/users', function () use ($userController): Response {
+    return $userController->index();
+});
+$router->post('/users/store', function (Request $request) use ($userController): Response {
+    return $userController->store($request);
+});
+
+// Deprecated
+$router->get('/profile', function () use ($userController): Response {
+    return $userController->getProfile();
+});
+$router->post('/profile/update', function (Request $request) use ($userController): Response {
+    return $userController->updateProfile($request);
+});
